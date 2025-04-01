238c78c421832419aa0cb32068283b3b12abd093
Siopapoo <john_christian_sayat@dlsu.edu.ph>
2025-04-01 13:24:28 +0800

    Delete Wolf.java

--- Wolf.java
+++
@@ -1,7 +0,0 @@
-package src.model.pieces;
-
-public class Wolf extends RegularPiece {
-    public Wolf(boolean isBlueTeam) {
-        super("Wolf", 3, isBlueTeam, isBlueTeam ? "/Assets/Pieces/blue_wolf.png" : "/Assets/Pieces/red_wolf.png");
-    }
-}
