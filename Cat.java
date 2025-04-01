8d865a27f15e5c2a74e27ec09904434f9e9915e9
Siopapoo <john_christian_sayat@dlsu.edu.ph>
2025-04-01 13:21:39 +0800

    Delete Cat.java

--- Cat.java
+++
@@ -1,7 +0,0 @@
-package src.model.pieces;
-
-public class Cat extends RegularPiece {
-    public Cat(boolean isBlueTeam) {
-        super("Cat", 1, isBlueTeam, isBlueTeam ? "/Assets/Pieces/blue_cat.png" : "/Assets/Pieces/red_cat.png");
-    }
-}
